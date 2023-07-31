e96558214077bef89279517a0db3ee84ed808ca3
CaoYuanpu <yuanpucao@gmail.com>
2023-07-31 20:45:53 +0800

    dev

--- metaicl/data.py
+++ metaicl/data.py
@@ -202,8 +202,16 @@ class MetaICLData(object):
             input_tokens = [input_tokens for _ in option_tokens]
             output_tokens = option_tokens
             option_tokens = [dp["options"].index(dp["output"])]
-            print(len(input_tokens), len(output_tokens), len(option_tokens))
-            input()
+            print("input")
+            for input_token in input_tokens:
+                print(input_token.shape)
+                input()
+
+            print("output")
+            for output_token in output_tokens:
+                print(output_token.shape)
+                input()
+
             if self.method=="direct":
                 return input_tokens, output_tokens, option_tokens
             elif self.method=="channel":
